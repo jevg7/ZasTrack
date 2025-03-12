12f46bb1c6a905ec3e3ef57b541878e165e0b4fb
Jairo <jvegag@uamv.edu.ni>
2025-03-11 23:01:46 -0600

    crear repo

---
+++ ZasTrack/wEstudiantes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZasTrack
+{
+    public partial class wEstudiantes : Form
+    {
+        public wEstudiantes()
+        {
+            InitializeComponent();
+        }
+
+        private void agregarEstudiantesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Abrir_Form(new wAgregarEstudiante());
+        }
+
+
+        private void Abrir_Form(object formhijo)
+        {
+
+            if (this.pnlCntEstudiantes.Controls.Count > 0)
+                this.pnlCntEstudiantes.Controls.RemoveAt(0); // Elimina cualquier control existente del panel contenedor.
+
+            Form fh = formhijo as Form; // Convierte el objeto de entrada en un formulario.
+            fh.TopLevel = false; // Establece la propiedad TopLevel del formulario como false.
+            fh.Dock = DockStyle.Fill; // Establece la propiedad Dock del formulario para que ocupe todo el espacio del panel contenedor.
+            this.pnlCntEstudiantes.Controls.Add(fh); // Agrega el formulario al panel contenedor.
+            this.pnlCntEstudiantes.Tag = fh; // Establece la propiedad Tag del panel contenedor como el formulario.
+            fh.Show(); // Muestra el formulario.
+        }
+    }
+}
---
+++ ZasTrack/wMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ZasTrack
+{
+    partial class wMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnProyecto = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnExamenes = new System.Windows.Forms.Button();
+            this.btnEstudiantes = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnProyecto);
+            this.panel1.Controls.Add(this.btnReportes);
+            this.panel1.Controls.Add(this.btnExamenes);
+            this.panel1.Controls.Add(this.btnEstudiantes);
+            this.panel1.Location = new System.Drawing.Point(-1, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 682);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnProyecto
+            // 
+            this.btnProyecto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProyecto.FlatAppearance.BorderSize = 0;
+            this.btnProyecto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProyecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProyecto.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnProyecto.Location = new System.Drawing.Point(0, 396);
+            this.btnProyecto.Margin = new System.Windows.Forms.Padding(2);
+            this.btnProyecto.Name = "btnProyecto";
+            this.btnProyecto.Size = new System.Drawing.Size(169, 68);
+            this.btnProyecto.TabIndex = 4;
+            this.btnProyecto.Text = "Proyecto";
+            this.btnProyecto.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProyecto.UseVisualStyleBackColor = true;
+            this.btnProyecto.Click += new System.EventHandler(this.btnProyecto_Click);
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnReportes.Location = new System.Drawing.Point(0, 314);
+            this.btnReportes.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(169, 68);
+            this.btnReportes.TabIndex = 3;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportes.UseVisualStyleBackColor = true;
+            this.btnReportes.Click += new System.EventHandler(this.btnReportes_Click);
+            // 
+            // btnExamenes
+            // 
+            this.btnExamenes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExamenes.FlatAppearance.BorderSize = 0;
+            this.btnExamenes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExamenes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExamenes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExamenes.Location = new System.Drawing.Point(0, 234);
+            this.btnExamenes.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExamenes.Name = "btnExamenes";
+            this.btnExamenes.Size = new System.Drawing.Size(169, 68);
+            this.btnExamenes.TabIndex = 2;
+            this.btnExamenes.Text = "Examenes";
+            this.btnExamenes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExamenes.UseVisualStyleBackColor = true;
+            this.btnExamenes.Click += new System.EventHandler(this.btnExamenes_Click);
+            // 
+            // btnEstudiantes
+            // 
+            this.btnEstudiantes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEstudiantes.FlatAppearance.BorderSize = 0;
+            this.btnEstudiantes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEstudiantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEstudiantes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEstudiantes.Location = new System.Drawing.Point(-2, 162);
+            this.btnEstudiantes.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEstudiantes.Name = "btnEstudiantes";
+            this.btnEstudiantes.Size = new System.Drawing.Size(169, 68);
+            this.btnEstudiantes.TabIndex = 1;
+            this.btnEstudiantes.Text = "Estudiantes";
+            this.btnEstudiantes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEstudiantes.UseVisualStyleBackColor = true;
+            this.btnEstudiantes.Click += new System.EventHandler(this.btnEstudiantes_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::ZasTrack.Properties.Resources.Zas_log;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlContenedor.Location = new System.Drawing.Point(171, 0);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(1015, 686);
+            this.pnlContenedor.TabIndex = 2;
+            // 
+            // wMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 685);
+            this.Controls.Add(this.pnlContenedor);
+            this.Controls.Add(this.panel1);
+            this.Name = "wMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnProyecto;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnExamenes;
+        private System.Windows.Forms.Button btnEstudiantes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnlContenedor;
+    }
+}
+
